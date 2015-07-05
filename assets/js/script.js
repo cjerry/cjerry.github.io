3f787fef8b4a31ecff9c60dd341deb1132453d7f
cjerry <jerry.c@berkeley.edu>
2015-07-05 17:15:40 -0600

    Production ready! Future changes will target expanding the experience section.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,43 @@
+// Make the project boxes equal height.
+function boxHeight(group) {
+    var tallest = 0;
+    group.each(function() {
+        var thisHeight = $(this).height();
+        if(thisHeight > tallest) {
+            tallest = thisHeight;
+        }
+    });
+    // Inject extra height into the short items.
+    group.each(function() { $(this).height(tallest); });
+}
+
 $(function() {
-    var shiftWindow = function() { scrollBy(0, -50) };
-    if (location.hash) shiftWindow();
-    window.addEventListener("hashchange", shiftWindow);
+    boxHeight($(".thumbnail"));
+    if ($(window).width() > 768) {
+        var lastScrollHeight = 0;
+        var lastSwitchHeight = 0;
+        $(window).scroll(function(event) {
+           var curTop = $(this).scrollTop();
+           if (curTop == 0) {
+               $(".navbar").css("padding", "20px 0");
+           }
+           if (lastScrollHeight - curTop > 0) {
+               // Scrolled up.
+               $(".navbar").stop().animate({ "marginTop" : "0px" }, 20);
+               if (curTop != 0) {
+                   $(".navbar").css("padding", "0");
+               } else {
+                   $(".navbar").css("padding", "20px 0");
+               }
+               lastSwitchHeight = curTop;
+           }
+           else if (curTop - lastSwitchHeight > 140) {
+               // Scrolled down significantly.
+               $(".navbar").css("padding", "0");
+               $(".navbar").stop().animate({ "marginTop" : "-50px" }, 100);
+               lastSwitchHeight = curTop;
+           }
+           lastScrollHeight = curTop;
+        });
+    }
 });
