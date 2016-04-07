6cc30fb3c2e0df8455b672de0592da30ef0fd257
Jerry Chen <jerry.c@berkeley.edu>
2016-04-07 15:51:33 -0700

    Add week 9

---
+++ assets/files/quiz9.scm
@@ -0,0 +1,11 @@
+; Quiz
+; What do the following evaluate to in the Scheme interpreter?
+; Example:
+; > '(1 . (2))
+; (1 2)
+
+(cons 1 (list (cons 2 3)))
+
+(cons 4 '(list (cons 5 6)))
+
+'(1 . (2 . (3 . (4))))
